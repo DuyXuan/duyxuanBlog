108ee6adce03607bc0b4afab21a9a09e74b00043
DuyXuan <duyxuan193@gmail.com>
2020-09-07 22:46:58 +0700

    fix form layout

--- Js/main.js
+++ Js/main.js
@@ -104,10 +104,10 @@ var emailInput = document.querySelector(".input__email");
 function checkEmpty() {
   for (var i = 0; i < input.length; i++) {
     if (input[i].value == "") {
-      errorMess[i].style.display = "block";
+      errorMess[i].style.opacity = 1;
       errorMess[i].textContent = "please input this field";
     } else {
-      errorMess[i].style.display = "none";
+      errorMess[i].style.opacity = 0;
     }
   }
 }
@@ -115,13 +115,13 @@ function checkEmpty() {
 
 function checkName() {
   if (nameInput.value.length > 10) {
-    errorMess[0].style.display = "block";
+    errorMess[0].style.opacity = 1;
     errorMess[0].textContent = "your name is too long";
   }
 }
 function checkPhone() {
   if (phoneInput.value.length > 12 || isNaN(phoneInput.value)) {
-    errorMess[2].style.display = "block";
+    errorMess[2].style.opacity = 1;
     errorMess[2].textContent = "phone must be number and < 12";
   }
 }
@@ -129,7 +129,7 @@ function checkPhone() {
 function checkEmail() {
   var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   if (emailInput.value != "" && regex.test(emailInput.value) == false) {
-    errorMess[1].style.display = "block";
+    errorMess[1].style.opacity = 1;
     errorMess[1].textContent = "email not valid";
   }
 }
